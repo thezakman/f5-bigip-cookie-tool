2ff906522bcbc18c6c5a522635e490a1cbd8b4da
Pedro Araujo <thezakman@icloud.com>
2023-05-17 15:31:24 -0300

    Fixed a mistake with the port encoding.

--- bigip-encode.py
+++ bigip-encode.py
@@ -1,21 +1,22 @@
+#!/usr/bin/env python3
 import sys
 
 ip, port = sys.argv[1], sys.argv[2]
+
 ip = ip.split('.')
 ip.reverse()
+
 enc_ip = []
 
 for i in ip:
-    enc_ip.append(hex(int(i))[2:])
+    enc_ip.append(hex(int(i))[2:].zfill(2))
 
-enc_ip = ''.join(map(lambda x: x.zfill(2), enc_ip))
-port = hex(int(port))[2:]
-enc_port = []
+enc_ip = ''.join(enc_ip)
+port = hex(int(port))[2:].zfill(4)
 
-for i in range(0, len(port), 2):
-    enc_port.append(str(port[i:i+2]))
+enc_port = ''.join([port[i:i+2] for i in range(0, len(port), 2)])
 
-enc_port.reverse()
-enc_port = ''.join(enc_port)
+encoded_ip = int(enc_ip, 16)
+encoded_port = int(enc_port, 16)
 
-print("Encoded BigIP Cookie: %s.%s.0000" % (int(enc_ip, 16), int(enc_port, 16)))
+print("Encoded BigIP Cookie: %s.%s.0000" % (encoded_ip, encoded_port))
